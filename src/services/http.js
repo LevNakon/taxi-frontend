5c93e01e9675b0878f4e2fd80a05c999ba542d30
LevNakon <lionsoul887@gmail.com>
2019-08-01 12:12:05 +0300

    RestAPI access config

--- src/services/http.js
+++ src/services/http.js
@@ -4,11 +4,12 @@ import auth from './auth';
 import LOCALSTORAGE from '../constants/localsorage';
 
 const http = axios.create({
-    baseURL: process.env.NODE_ENV !== 'production'? 'http://localhost:8080' : 'https://taxi-like-backend.herokuapp.com'
+    baseURL: process.env.NODE_ENV !== 'production' ? 'http://localhost:8080' : 'https://taxi-like-backend.herokuapp.com'
 });
 
 http.interceptors.request.use((config) => {
-    if (localStorage.getItem(LOCALSTORAGE.TOKEN)!==null) {
+    console.log('baseUrl', http.baseURL);
+    if (localStorage.getItem(LOCALSTORAGE.TOKEN) !== null) {
         let current_time = Date.now() / 1000;
         let expiration_time = auth.parseJwt(localStorage.getItem(LOCALSTORAGE.TOKEN)).exp;
         if (expiration_time < current_time) {
